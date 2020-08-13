5773c97caf716794f4629272151c02f4591af00c
Leslie Huynh <69611016+lh81417@users.noreply.github.com>
2020-08-13 12:47:29 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Leslie Huynh");
+		System.out.println("I like to watch Korean Dramas");
+		System.out.println("I want to go to SJSU");
+		System.out.println("I really like cats");
+		System.out.println("I want to become a nurse");
+		
+	}
+}
